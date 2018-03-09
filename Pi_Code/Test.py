b72c075b3888470747242d1e6edd68b4b9030b56
Robbie Lowles <robbie.windsor+git@gmail.com>
2018-03-08 21:56:29 -0500

    Updates to test

--- Pi_Code/Test.py
+++ Pi_Code/Test.py
@@ -1,6 +1,8 @@
 from Pi_Code import Submarine
-
+import time
 #Test script to see if the controller works. No
+
+time.sleep(10)
 
 sub = Submarine.Submarine(False)
 
@@ -8,6 +10,7 @@ while True:
 
 	if sub._state == "idle":
 		sub.UpdateJoystick()
-
+	if sub._state == "auto":
+		sub.Forward(15)
 	if sub._state == "manual":
 		sub.UpdateJoystick()
